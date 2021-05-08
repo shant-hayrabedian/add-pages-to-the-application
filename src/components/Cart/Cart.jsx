da3a18ad0ac0fccef463b2b1f688db48677909fc
shant <shant.shekhook@gmail.com>
2021-05-08 11:46:37 +0400

    Cart added

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,8 @@
+import './Cart.css';
+
+
+const Cart = () => {
+
+}
+
+export default Cart;
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -26,23 +26,25 @@ const ProductList = () => {
     return (
         <div className="blog-list">
             {products.map(product => {
-                    return (
-                        <Link to={`/products/${product.id}`} key={product.id}>
-                            <CardDeck style={{display: 'flex', flexDirection: 'row'}}>
-                                <Card style={{width: '30rem', border: '0px', display: 'flex'}}>
+                return (
 
-                                    <Card.Title className="itemName">{product.id} - {product.name}</Card.Title>
-                                    {!product.image && (
-                                        <Card.Img src='/assets/default-image-620x600.jpg' className="noImages"/>
-                                    )}
-                                    <Card.Img src={product.image} className="itemImages"/>
-                                    <Button className="CartFooterButton" variant="info" size="lg">Add To Cart</Button>
+                    <CardDeck style={{display: 'flex', flexDirection: 'row'}} key={product.id}>
+                        <Card style={{width: '30rem', border: '0px', display: 'flex'}}>
+                            <Link
+                                to={`/products/${product.id}`}>
+                                <Card.Title className="itemName">{product.id} - {product.name}</Card.Title>
+                                {!product.image && (
+                                    <Card.Img src='/assets/default-image-620x600.jpg' className="noImages"/>
+                                )}
+                                <Card.Img src={product.image} className="itemImages"/>
+                            </Link>
+                            <Button className="CartFooterButton" variant="info" size="lg">Add To Cart</Button>
+                        </Card>
 
-                                </Card>
-                            </CardDeck>
-                        </Link>
-                    )
-                }
+                    </CardDeck>
+
+                )
+            }
             )}
             {/*<Switch>*/}
             {/*    <Route path="/products/:id" component={Product} />*/}
